7df29a72478b24ef1ac51ec9860c53833dad06fa
Kemal Ogün Işık <ogunisik@gmail.com>
2016-01-07 22:05:14 +0200

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(3, 5));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
